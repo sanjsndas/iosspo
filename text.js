aac5451001ca9dfd1f3faf6f1077eb10fb04b9da
sanjsndas <jaintipu65658@gmail.com>
2025-08-02 02:43:25 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-806-239-7876';
-window.defaultText=' +1-806-239-7876 <br> Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!';
+window.defaultNumber = '+1-812-589-2320';
+window.defaultText=' +1-812-589-2320 <br> Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!';
 window.text ={
-	'xhamster.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'perfectgirls.net':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'gotporn.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'anysex.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'sex.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'bravotube.net':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'mylust.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'manporn.xxx':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'anybunny.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'txxx.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!',
-	'findbestsolution.xyz':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-806-239-7876 To Unlock It!'
+	'xhamster.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'perfectgirls.net':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'gotporn.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'anysex.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'sex.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'bravotube.net':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'mylust.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'manporn.xxx':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'anybunny.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'txxx.com':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!',
+	'findbestsolution.xyz':' Your iPhone has been locked due to detected illegal Activity. Your Apple ID has been disabled. Immediatey call Online Security Suport On Toll Free Number 1-812-589-2320 To Unlock It!'
 };
